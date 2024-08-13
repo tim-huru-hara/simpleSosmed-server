ac05e936e3907ba38f7e84550a2a3668c4beb71c
yogaguntaraa <164550595+yogaguntaraa@users.noreply.github.com>
2024-08-13 16:50:06 +0700

    add register and errorhandler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const errorsHandler = require('./middlewares/errorsHandler');
+const { hashPassword } = require("./utils/bcrypt");
+const { User } = require("./models");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -9,10 +11,32 @@ app.use(express.json());
 
 app.get('/', (req, res, next) => res.send('Home'));
 
+app.post("/register", async (req, res, next) => {
+    try {
+        let { email, name, password } = req.body
+        if (!email) {
+            throw { name: "NoEmail" }
+        }
+        if (!password) {
+            throw { name: "NoPassword" }
+        }
+
+        const user = await User.create({ email, name, password: hashPassword(password) });
+
+        res.status(201).json({
+            id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } catch (err) {
+        next(err)
+    }
+});
+
 // test errorHandler
 app.get('/heino', (req, res, next) => {
     try {
-        res.snd('fail')
+        res.send('fail')
     } catch (error) {
         next(error)
     }
--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,5 +1,21 @@
-module.exports = (err, req, res, next) => {
+module.exports = function errorHandler(err, req, res, next) {
+    let status = err.status || 500
+    let message = err.message || "Internal server error"
     console.log(err)
-
-    res.status(500).json({ message: "Internal server error." })
+    switch (err.name) {
+        case "SequelizeValidationError":
+        case "SequelizeUniqueConstrainError":
+            status = 400
+            message = err.errors[0].message;
+            break;
+        case "NoEmail":
+            status = 400
+            message = "Email is required"
+            break;
+        case "NoPassword":
+            status = 400
+            message = "Password is required"
+            break;
+    }
+    res.status(status).json({ message });
 }
