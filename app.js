2b5499a519b231943e05405909974427d1529ff5
yogaguntaraa <164550595+yogaguntaraa@users.noreply.github.com>
2024-08-14 15:14:31 +0700

    tidy up app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res, next) => res.send("Home"));
 app.post("/register", register);
 app.post("/login", login);
 app.post("/post", authentication, postPost);
@@ -27,14 +26,6 @@ app.get("/post", getPosts);
 app.get("/userInfo", authentication, getInfo);
 app.post("/likes/:id", authentication, addLike);
 app.post("/comments/:id", authentication, addComment);
-// test errorHandler
-app.get("/heino", (req, res, next) => {
-  try {
-    res.send("fail");
-  } catch (error) {
-    next(error);
-  }
-});
 app.put("/user/edit", authentication, EditProfile);
 
 app.use(errorsHandler);
