bf0dba2dcfb73a8d3b078012c88a89313ee232f0
putram11 <putram.works@gmail.com>
2024-08-14 11:48:39 +0700

    git like updateController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,33 +1,38 @@
-const { Post, User } = require('../models')
+const { Post, User } = require("../models");
 
 module.exports = class {
-    static async postPost(req, res, next) {
-        const { imageUrl, description } = req.body
-        try {
-            await Post.create({
-                userId: req.user.id,
-                description,
-                imageUrl
-            })
-            res.status(200).json({ message: 'posted' })
-        } catch (error) {
-            console.log(error)
-            next(error)
-        }
+  static async postPost(req, res, next) {
+    const { imageUrl, description } = req.body;
+    try {
+      await Post.create({
+        userId: req.user.id,
+        description,
+        imageUrl,
+      });
+      res.status(200).json({ message: "posted" });
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
+  }
 
-    static async getPosts(req, res, next) {
-        try {
-            const posts = await Post.findAll({
-                order: [['createdAt', 'DESC']],
-                include: {
-                    model: User,
-                    attributes: ['username', 'imageUrl']
-                }
-            });
-            res.status(200).json(posts)
-        } catch (error) {
-            next(error)
-        }
+  static async getPosts(req, res, next) {
+    try {
+      const posts = await Post.findAll({
+        order: [["createdAt", "DESC"]],
+        include: {
+          model: User,
+          attributes: ["username", "imageUrl"],
+        },
+      });
+      const postsWithLikeCount = posts.map((post) => ({
+        ...post.toJSON(),
+        likeCount: post.likes ? post.likes.length : 0,
+      }));
+
+      res.status(200).json(postsWithLikeCount);
+    } catch (error) {
+      next(error);
     }
-}
+  }
+};
