4f3b9dc56bab90ae7b47ec811da06cd825f7aa8e
febriantopermana <febriantopermana16@gmail.com>
2024-08-14 10:29:58 +0700

    EditProfile-Auth-separation done 2

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const { hashPassword, comparePassword } = require("./utils/bcrypt");
 const { User } = require("./models");
 const { signToken, verifyToken } = require("./utils/jwt");
 const { postPost, getPosts } = require('./controllers/postController');
-const { getInfo } = require('./controllers/userController');
+const { getInfo, EditProfile } = require('./controllers/userController');
+const authentication = require('./middlewares/authentication');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -70,9 +71,9 @@ app.post("/login", async (req, res, next) => {
 });
 
 // rofiq
-app.post('/post', auth, postPost)
+app.post('/post', authentication, postPost)
 app.get('/post', getPosts)
-app.get('/userInfo', auth, getInfo)
+app.get('/userInfo', authentication, getInfo)
 
 // test errorHandler
 app.get('/heino', (req, res, next) => {
@@ -83,55 +84,7 @@ app.get('/heino', (req, res, next) => {
     }
 })
 
-// auth function
-async function auth(req, res, next) {
-    try {
-        const access_token = req.headers.authorization
-        if (!access_token) throw { name: "InvalidToken" };
-
-        const [type, token] = access_token.split(' ');
-        if (type !== `Bearer`) throw { name: `InvalidToken` }
-
-        const payload = verifyToken(token)
-
-        const user = await User.findByPk(payload.id)
-        if (!user) throw { name: `InvalidToken` }
-
-        req.user = {
-            id: user.id
-        }
-
-        next()
-    } catch (error) {
-        next(error)
-    }
-}
-
-//authentication --Febri
-app.use(auth)
-
-
-// edit user --Febri
-app.put('/user/edit', async (req, res, next) => {
-    try {
-        const { id } = req.user;
-
-        const { username, password, bio, imageUrl } = req.body;
-        if (!username) throw { name: "EmptyUsername" }
-        if (!password) throw { name: "EmptyPassword" }
-        if (!bio) throw { name: "EmptyBio" }
-        if (!imageUrl) throw { name: "EmptyImageUrl" }
-
-        const user = await User.findByPk(id)
-        if (!user) throw { name: "UserNotFound" }
-
-        const editUser = await user.update({ username, password: hashPassword(password), bio, imageUrl })
-
-        res.status(201).json({ message: "User update success" })
-    } catch (error) {
-        next(error)
-    }
-})
+app.put('/user/edit', authentication, EditProfile)
 
 app.use(errorsHandler)
 module.exports = app;
